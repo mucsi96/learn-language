366af9062a924dcc1acb08e67f962e590edb50a9
Igor Bari <mucsi96@gmail.com>
2024-11-10 12:27:32 +0100

    add custom theme

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material/core';
+
+const globalRippleConfig: RippleGlobalOptions = {
+  disabled: true,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(),
+    { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig },
+    provideAnimationsAsync(),
   ],
 };
