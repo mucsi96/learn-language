eefd32a2b00f5708cf473373f8ea56dae60903bb
Igor Bari <mucsi96@gmail.com>
2024-12-13 08:19:38 +0100

    feat: refactor card component to use CardService for data management; remove WordService and streamline example handling

---
+++ client/src/app/card.service.ts
@@ -0,0 +1,118 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  computed,
+  inject,
+  Injectable,
+  linkedSignal,
+  resource,
+  signal,
+} from '@angular/core';
+import { Translation, Word } from './parser/types';
+import { fetchJson } from './utils/fetchJson';
+
+export const languages = ['hu', 'ch', 'en'] as const;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CardService {
+  readonly selectedWord = signal<Word | undefined>(undefined);
+  private readonly http = inject(HttpClient);
+
+  readonly type = signal('');
+  readonly word = linkedSignal(() => this.selectedWord()?.word);
+  readonly translationMap = Object.fromEntries(
+    languages.map((languageCode) => [
+      languageCode,
+      resource<Translation | undefined, { selectedWord: Word | undefined }>({
+        request: () => ({ selectedWord: this.selectedWord() }),
+        loader: async ({ request: { selectedWord } }) => {
+          if (!selectedWord) {
+            return;
+          }
+
+          return fetchJson<Translation>(
+            this.http,
+            `/api/translate/${languageCode}`,
+            {
+              body: selectedWord,
+              method: 'POST',
+            }
+          );
+        },
+      }),
+    ])
+  );
+  readonly translation = Object.fromEntries(
+    languages.map((languageCode) => [
+      languageCode,
+      linkedSignal(
+        () => this.translationMap[languageCode].value()?.translation
+      ),
+    ])
+  );
+  readonly forms = linkedSignal(() =>
+    this.selectedWord()?.forms.map((form) => signal(form))
+  );
+  readonly examples = linkedSignal(() =>
+    this.selectedWord()?.examples.map((example) => signal(example))
+  );
+  readonly examplesTranslations = linkedSignal(() => {
+    const examples = this.selectedWord()?.examples;
+
+    if (!examples) {
+      return;
+    }
+
+    return Object.fromEntries(
+      languages.map((languageCode) => [
+        languageCode,
+        examples.map((_, index) =>
+          linkedSignal(
+            () => this.translationMap[languageCode].value()?.examples[index]
+          )
+        ),
+      ])
+    );
+  });
+  readonly exampleImages = resource<
+    string[],
+    { id?: string; examples?: string[] }
+  >({
+    request: () => ({
+      id: this.selectedWord()?.id,
+      examples: this.selectedWord()?.examples,
+    }),
+    loader: async ({ request: { id, examples } }) => {
+      if (!id || !examples) {
+        return [];
+      }
+
+      return await Promise.all(
+        examples.map(async (example, index) => {
+          const { url } = await fetchJson<{ url: string }>(
+            this.http,
+            `/api/image`,
+            {
+              body: { id, input: example, index },
+              method: 'POST',
+            }
+          );
+          return url;
+        })
+      );
+    },
+  });
+
+  async selectWord(word: Word) {
+    this.selectedWord.set(word);
+  }
+
+  get isLoading() {
+    return computed(() =>
+      Object.values(this.translationMap).some((translation) =>
+        translation.isLoading()
+      )
+    );
+  }
+}
--- client/src/app/parser/card/card.component.ts
+++ client/src/app/parser/card/card.component.ts
@@ -1,22 +1,14 @@
-import {
-  Component,
-  effect,
-  inject,
-  linkedSignal,
-  resource,
-  signal,
-} from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { languages, WordService } from '../../word.service';
-import { Params } from '@angular/router';
-import { queryParamToObject } from '../../utils/queryCompression';
-import { Word } from '../types';
 import { injectParams } from '../../utils/inject-params';
+import { queryParamToObject } from '../../utils/queryCompression';
+import { CardService } from '../../card.service';
+import { Word } from '../types';
 
 @Component({
   selector: 'app-card',
@@ -33,70 +25,16 @@ import { injectParams } from '../../utils/inject-params';
   styleUrl: './card.component.css',
 })
 export class CardComponent {
-  private readonly wordService = inject(WordService);
+  private readonly cardService = inject(CardService);
   private readonly cardData = injectParams<string>('cardData');
-  readonly loading = this.wordService.isLoading;
-  readonly type = signal('');
-  readonly word = linkedSignal(() => this.wordService.selectedWord()?.word);
-  readonly translation = Object.fromEntries(
-    languages.map((languageCode) => [
-      languageCode,
-      linkedSignal(
-        () => this.wordService.translation[languageCode].value()?.translation
-      ),
-    ])
-  );
-  readonly forms = linkedSignal(() =>
-    this.wordService.selectedWord()?.forms.map((form) => signal(form))
-  );
-  readonly examples = linkedSignal(() =>
-    this.wordService.selectedWord()?.examples.map((example) => signal(example))
-  );
-  readonly examplesTranslations = linkedSignal(() => {
-    const examples = this.wordService.selectedWord()?.examples;
-
-    if (!examples) {
-      return;
-    }
-
-    return Object.fromEntries(
-      languages.map((languageCode) => [
-        languageCode,
-        examples.map((_, index) =>
-          linkedSignal(
-            () =>
-              this.wordService.translation[languageCode].value()?.examples[
-                index
-              ]
-          )
-        ),
-      ])
-    );
-  });
-  readonly exampleImages = resource<
-    string[],
-    { id?: string; examples?: string[] }
-  >({
-    request: () => ({
-      id: this.wordService.selectedWord()?.id,
-      examples: this.examples()?.map((example) => example()),
-    }),
-    loader: async ({ request: { id, examples } }) => {
-      if (!id || !examples) {
-        return [];
-      }
-
-      return await Promise.all(
-        examples.map((example, index) =>
-          this.wordService.createImage({
-            id,
-            input: example,
-            index,
-          })
-        )
-      );
-    },
-  });
+  readonly loading = this.cardService.isLoading;
+  readonly word = this.cardService.word;
+  readonly type = this.cardService.type;
+  readonly translation = this.cardService.translation;
+  readonly forms = this.cardService.forms;
+  readonly examples = this.cardService.examples;
+  readonly examplesTranslations = this.cardService.examplesTranslations;
+  readonly exampleImages = this.cardService.exampleImages;
 
   constructor() {
     effect(async () => {
@@ -107,7 +45,7 @@ export class CardComponent {
       }
 
       const word = await queryParamToObject<Word>(cardData);
-      this.wordService.selectWord(word);
+      this.cardService.selectWord(word);
     });
   }
 }
