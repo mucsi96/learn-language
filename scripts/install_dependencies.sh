c4e03b83f8427a9577a707cc7dacffc13d5d1eb0
Igor Bari <mucsi96@gmail.com>
2025-01-31 21:40:18 +0100

    feat: add task to install dependencies and update Azure credentials retrieval

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,11 +5,10 @@ set -e  # Exit immediately if a command exits with a non-zero status
 db_k8s_config=$(az keyvault secret show --vault-name p05 --name db-namespace-k8s-user-config --query value -o tsv)
 db_username=$(az keyvault secret show --vault-name p05 --name db-username --query value -o tsv)
 db_password=$(az keyvault secret show --vault-name p05 --name db-password --query value -o tsv)
-azure_subscription_id=$(terraform output -raw subscription_id)
-azure_tenant_id=$(terraform output -raw tenant_id)
-admin_api_client_id=$(terraform output -raw admin_api_client_id)
-admin_api_client_secret=$(terraform output -raw admin_api_client_secret)
-admin_spa_client_id=$(terraform output -raw admin_spa_client_id)
+azure_subscription_id=$(az account show --query id --output tsv)
+azure_tenant_id=$(az keyvault secret show --vault-name p05 --name tenant-id --query value -o tsv)
+api_client_id=$(az keyvault secret show --vault-name p05 --name learn-language-api-client-id --query value -o tsv)
+spa_client_id=$(az keyvault secret show --vault-name p05 --name learn-language-spa-client-id --query value -o tsv)
 
 mkdir -p .kube
 echo "$db_k8s_config" > .kube/db-config
@@ -19,11 +18,10 @@ echo "DB_USERNAME=$db_username" >> .env
 echo "DB_PASSWORD=$db_password" >> .env
 echo "AZURE_SUBSCRIPTION_ID=$azure_subscription_id" >> .env
 echo "AZURE_TENANT_ID=$azure_tenant_id" >> .env
-echo "AZURE_CLIENT_ID=$admin_api_client_id" >> .env
-echo "AZURE_CLIENT_SECRET=$admin_api_client_secret" >> .env
+echo "AZURE_CLIENT_ID=$api_client_id" >> .env
 echo "NG_APP_TENANT_ID=$azure_tenant_id" > client/.env
-echo "NG_APP_CLIENT_ID=$admin_spa_client_id" >> client/.env
-echo "NG_APP_API_CLIENT_ID=$admin_api_client_id" >> client/.env
+echo "NG_APP_CLIENT_ID=$spa_client_id" >> client/.env
+echo "NG_APP_API_CLIENT_ID=$api_client_id" >> client/.env
 
 cd client && npm install && cd ..
 
