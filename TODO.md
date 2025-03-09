ff9d1026c55bc42364de18b2ff81d30c21c0ad2b
Igor Bari <mucsi96@gmail.com>
2025-03-09 16:39:42 +0100

    Add scheduling parameters to cards table

--- TODO.md
+++ TODO.md
@@ -1,5 +1,7 @@
+
+https://github.com/open-spaced-repetition/py-fsrs/blob/main/fsrs/fsrs.py
+
 - Complete the missing verb forms for the A2 level.  
 - Enable text selection without interfearing with draggign
 - Create a route for player
 - Create api for sheduler
-- Add scheduling parameters to cards table
--- server/database.py
+++ server/database.py
@@ -1,9 +1,11 @@
 from os import environ
-from sqlalchemy import ForeignKey, MetaData, create_engine, JSON
+from sqlalchemy import ForeignKey, MetaData, create_engine, JSON, Integer, Float, DateTime, Enum
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.schema import CreateSchema
 from sqlalchemy import event
 from urllib.parse import quote_plus
+from enum import IntEnum
+from datetime import datetime
 
 username = quote_plus(environ.get("DB_USERNAME"))
 password = quote_plus(environ.get("DB_PASSWORD"))
@@ -12,10 +14,8 @@ engine = create_engine(
     f"postgresql+psycopg://{username}:{password}@localhost:8484/postgres1", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 def get_db():
     yield SessionLocal()
-
 
 class Base(DeclarativeBase):
     metadata = MetaData(
@@ -24,10 +24,27 @@ class Base(DeclarativeBase):
     
 event.listen(Base.metadata, 'before_create', CreateSchema(Base.metadata.schema, if_not_exists=True))
 
+class State(IntEnum):
+    Learning = 1
+    Review = 2
+    Relearning = 3
+
+class Rating(IntEnum):
+    Again = 1
+    Hard = 2
+    Good = 3
+    Easy = 4
+
 class Card(Base):
     __tablename__ = "cards"
     id: Mapped[str] = mapped_column(primary_key=True)
-    data: Mapped[JSON] = mapped_column(type_=JSON)
+    data: Mapped[JSON] = mapped_column(type_=JSON, nullable=False)
+    state: Mapped[State] = mapped_column(Enum(State), nullable=False)
+    step: Mapped[int] = mapped_column(type_=Integer, nullable=True)
+    stability: Mapped[float] = mapped_column(type_=Float, nullable=True)
+    difficulty: Mapped[float] = mapped_column(type_=Float, nullable=True)
+    due: Mapped[datetime] = mapped_column(type_=DateTime, nullable=False)
+    last_review: Mapped[datetime] = mapped_column(type_=DateTime, nullable=True)
 
 class Source(Base):
     __tablename__ = "sources"
@@ -43,10 +60,17 @@ class CardSource(Base):
     source_id: Mapped[str] = mapped_column(ForeignKey("learn_language.sources.id"), primary_key=True)
     page_number: Mapped[int] = mapped_column(nullable=False)
 
+class ReviewLog(Base):
+    __tablename__ = "review_logs"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    card_id: Mapped[str] = mapped_column(ForeignKey("learn_language.cards.id"), nullable=False)
+    rating: Mapped[Rating] = mapped_column(Enum(Rating), nullable=False)
+    review_datetime: Mapped[datetime] = mapped_column(type_=DateTime, nullable=False)
+    review_duration: Mapped[int] = mapped_column(type_=Integer, nullable=True)
+
 def create_all():
     Base.metadata.create_all(engine, checkfirst=True)
     print("Tables created")
-
 
 if __name__ == "__main__":
     create_all()
