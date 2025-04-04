ee80334aa7408e81eded1accfdeda97cd64f5145
Igor Bari <mucsi96@gmail.com>
2025-04-04 08:41:42 +0200

    start migrating server to java

---
+++ server/src/main/java/io/github/mucsi96/learnlanguage/LearnlanguageApplication.java
@@ -0,0 +1,13 @@
+package io.github.mucsi96.learnlanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnlanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnlanguageApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/mucsi96/learnlanguage/LearnlanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mucsi96.learnlanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnlanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
