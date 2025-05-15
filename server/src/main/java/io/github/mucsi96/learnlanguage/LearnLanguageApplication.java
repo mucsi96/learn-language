75facdd0c8ce2368341860dea68a9c62c3c6257d
Igor Bari <mucsi96@gmail.com>
2025-05-15 21:21:22 +0200

    Refactor application startup logic and update VS Code settings for improved development environment

--- server/src/main/java/io/github/mucsi96/learnlanguage/LearnLanguageApplication.java
+++ server/src/main/java/io/github/mucsi96/learnlanguage/LearnLanguageApplication.java
@@ -3,27 +3,10 @@ package io.github.mucsi96.learnlanguage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class LearnLanguageApplication {
 
 	public static void main(String[] args) {
-		String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
-		if ("local".equals(activeProfile)) {
-			try {
-				new ProcessBuilder("kubectl", "port-forward", "services/postgres1",
-						System.getenv("DB_PORT") + ":http",
-						"--kubeconfig", "../.kube/db-config",
-						"--namespace", "db")
-						.inheritIO()
-						.start();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 		SpringApplication.run(LearnLanguageApplication.class, args);
-
 	}
-
 }
