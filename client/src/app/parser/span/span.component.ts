330292dce440000d8ba84b97a397e7e37be40685
Igor Bari <mucsi96@gmail.com>
2025-06-05 12:46:59 +0200

    signal-input-migration

--- client/src/app/parser/span/span.component.ts
+++ client/src/app/parser/span/span.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, inject, Input } from '@angular/core';
+import { Component, HostBinding, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BBox, WordList } from '../types';
@@ -23,57 +23,57 @@ import { CompressQueryPipe } from '../../utils/compress-query.pipe';
   styleUrl: './span.component.css',
 })
 export class SpanComponent {
-  @Input() sourceId?: string;
-  @Input() pageNumber?: number;
-  @Input() text?: string;
-  @Input() font?: string;
-  @Input() fontSize?: string;
-  @Input() color?: string;
-  @Input() bbox?: BBox;
-  @Input() searchTerm?: string;
-  @Input() exists?: boolean;
-  @Input() wordList?: WordList;
+  readonly sourceId = input<string>();
+  readonly pageNumber = input<number>();
+  readonly text = input<string>();
+  readonly font = input<string>();
+  readonly fontSize = input<string>();
+  readonly color = input<string>();
+  readonly bbox = input<BBox>();
+  readonly searchTerm = input<string>();
+  readonly exists = input<boolean>();
+  readonly wordList = input<WordList>();
   readonly dialog = inject(MatDialog);
 
   get matches() {
     if (
-      !this.searchTerm ||
-      (this.bbox?.y ?? 0) < (this.wordList?.y ?? 0) ||
-      (this.bbox?.y ?? 0) >
-        (this.wordList?.y ?? 0) + (this.wordList?.height ?? 0) ||
-      (this.bbox?.x ?? 0) < (this.wordList?.x ?? 0) ||
-      (this.bbox?.x ?? 0) >
-        (this.wordList?.x ?? 0) + (this.wordList?.width ?? 0)
+      !this.searchTerm() ||
+      (this.bbox()?.y ?? 0) < (this.wordList()?.y ?? 0) ||
+      (this.bbox()?.y ?? 0) >
+        (this.wordList()?.y ?? 0) + (this.wordList()?.height ?? 0) ||
+      (this.bbox()?.x ?? 0) < (this.wordList()?.x ?? 0) ||
+      (this.bbox()?.x ?? 0) >
+        (this.wordList()?.x ?? 0) + (this.wordList()?.width ?? 0)
     ) {
       return [];
     }
 
-    return this.wordList?.words?.filter((word) =>
-      word.word.toLowerCase().includes(this.searchTerm!.toLowerCase())
+    return this.wordList()?.words?.filter((word) =>
+      word.word.toLowerCase().includes(this.searchTerm()!.toLowerCase())
     );
   }
 
   @HostBinding('style.top') get top() {
-    return `calc(var(--page-width) * ${this.bbox?.y ?? 0})`;
+    return `calc(var(--page-width) * ${this.bbox()?.y ?? 0})`;
   }
 
   @HostBinding('style.left') get left() {
-    return `calc(var(--page-width) * ${this.bbox?.x ?? 0})`;
+    return `calc(var(--page-width) * ${this.bbox()?.x ?? 0})`;
   }
 
   @HostBinding('style.width') get width() {
-    return `calc(var(--page-width) * ${this.bbox?.width ?? 0})`;
+    return `calc(var(--page-width) * ${this.bbox()?.width ?? 0})`;
   }
 
   @HostBinding('style.height') get height() {
-    return `calc(var(--page-width) * ${this.bbox?.height ?? 0})`;
+    return `calc(var(--page-width) * ${this.bbox()?.height ?? 0})`;
   }
 
   get cardHrefPrefix() {
-    return `/sources/${this.sourceId}/page/${this.pageNumber}/cards?cardData=`;
+    return `/sources/${this.sourceId()}/page/${this.pageNumber()}/cards?cardData=`;
   }
 
   get ariaDescription() {
-    return this.exists ?  'Card exists' : 'Card does not exist';
+    return this.exists() ?  'Card exists' : 'Card does not exist';
   }
 }
