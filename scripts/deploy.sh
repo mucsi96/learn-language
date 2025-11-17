b0034dfb5ac2ed767824725161a5f931eb3331cb
Igor Bari <mucsi96@gmail.com>
2025-11-17 09:52:46 +0100

    update deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,25 +18,29 @@ if [ "$(uname -s)" = "Linux" ] && [ -f /etc/os-release ]; then
     fi
 fi
 
-host=$(az keyvault secret show --vault-name p06 --name hostname --query value --output tsv)
+dnsZone=$(az keyvault secret show --vault-name p06 --name dns-zone --query value --output tsv)
 apiClientId=$(az keyvault secret show --vault-name p06 --name learn-language-api-client-id --query value --output tsv)
 spaClientId=$(az keyvault secret show --vault-name p06 --name learn-language-spa-client-id --query value --output tsv)
 db_username=$(az keyvault secret show --vault-name p06 --name db-username --query value -o tsv)
 db_password=$(az keyvault secret show --vault-name p06 --name db-password --query value -o tsv)
 openai_api_key=$(az keyvault secret show --vault-name p06 --name learn-language-openai-api-key --query value -o tsv)
+langsmith_api_key=$(az keyvault secret show --vault-name p06 --name learn-language-langsmith-api-key --query value -o tsv)
+google_ai_api_key=$(az keyvault secret show --vault-name p06 --name learn-language-google-ai-api-key --query value -o tsv)
+eleven_labs_api_key=$(az keyvault secret show --vault-name p06 --name learn-language-eleven-labs-api-key --query value -o tsv)
 latestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/learn-language/tags/" | jq -r '.results |  map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
 
-echo "Deploying mucsi96/learn-language:$latestTag to language.$host"
+echo "Deploying mucsi96/learn-language:$latestTag to language.$dnsZone"
 
 helm repo update
 
 helm upgrade learn-language mucsi96/spring-app \
     --install \
-    --force \
+    --force-replace \
     --kubeconfig .kube/config \
     --namespace learn-language \
     --set image=mucsi96/learn-language:$latestTag \
-    --set host=language.$host \
+    --set host=language.$dnsZone \
+    --set entryPoint=web \
     --set clientId=$apiClientId \
     --set serviceAccountName=learn-language-api-workload-identity \
     --set env.STORAGE_DIRECTORY=/app/storage \
@@ -46,5 +50,12 @@ helm upgrade learn-language mucsi96/spring-app \
     --set env.DB_USERNAME=$db_username \
     --set env.DB_PASSWORD=$db_password \
     --set env.OPENAI_API_KEY=$openai_api_key \
+    --set env.LANGSMITH_API_KEY=$langsmith_api_key \
+    --set env.GOOGLE_AI_API_KEY=$google_ai_api_key \
+    --set env.ELEVEN_LABS_API_KEY=$eleven_labs_api_key \
     --set env.UI_CLIENT_ID=$spaClientId \
+    --set persistentVolumeClaims[0].name=learn-language \
+    --set persistentVolumeClaims[0].mountPath=/app/storage \
+    --set persistentVolumeClaims[0].accessMode=ReadWriteMany \
+    --set persistentVolumeClaims[0].storage=5Gi \
     --wait
