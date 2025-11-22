f13d44106abd70fc558159b30b7acf3a98cb90d2
Igor Bari <mucsi96@gmail.com>
2025-11-22 11:54:22 +0100

    feat: migrate database initialization to schema.sql and update references in compose and utils

--- test/utils.ts
+++ test/utils.ts
@@ -137,7 +137,7 @@ export async function cleanupDbRecords({ withSources }: { withSources?: boolean 
 
 export async function populateDb(): Promise<void> {
   await withDbConnection(async (client) => {
-    const initSqlPath = path.join(__dirname, '..', 'test', 'init.sql');
+    const initSqlPath = path.join(__dirname, '..', 'server', 'src', 'main', 'resources', 'schema.sql');
     const initSql = fs.readFileSync(initSqlPath, 'utf-8');
     await client.query(initSql);
   });
