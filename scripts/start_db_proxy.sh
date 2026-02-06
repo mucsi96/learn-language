3cb34d813c1cd6b477e320ed6f978fa5dec2fa25
Igor Bari <mucsi96@gmail.com>
2026-02-06 08:57:41 +0100

    fix: update database schema by removing unnecessary columns and adjusting data types

--- scripts/start_db_proxy.sh
+++ scripts/start_db_proxy.sh
@@ -4,4 +4,4 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 KUBECONFIG=$(az keyvault secret show --vault-name p06 --name db-namespace-k8s-user-config --query value -o tsv)
 
-kubectl port-forward services/postgres1 5461:http --namespace db
+kubectl port-forward services/postgres1 5461:http --namespace db --kubeconfig <(echo "$KUBECONFIG")
