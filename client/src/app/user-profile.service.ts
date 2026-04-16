44c6e703873e921520e418ebd5279a5f5db5b1cc
Igor Bari <mucsi96@gmail.com>
2026-04-16 20:19:10 +0200

    Refactor UserProfileService to improve profile computation and code clarity

--- client/src/app/user-profile.service.ts
+++ client/src/app/user-profile.service.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable } from '@angular/core';
+import { computed, Injectable, inject } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -7,18 +7,27 @@ import { AuthService } from './auth.service';
 export class UserProfileService {
   private readonly authService = inject(AuthService);
 
-  readonly profile = computed(() => {
-    const userData = this.authService.userData();
-    const name = userData?.name as string | undefined;
-    if (!name) return undefined;
-    return { name, initials: this.getInitials(name) };
+  profile = computed(() => {
+    const userDataResult = this.authService.userData();
+    const userData = userDataResult?.userData;
+    const name = userData?.name ?? userData?.preferred_username;
+
+    if (!name) {
+      return undefined;
+    }
+
+    return {
+      name,
+      initials: this.getInitials(name),
+    };
   });
 
-  private getInitials(name: string): string {
-    return name
+  private getInitials(name: string | undefined): string {
+    if (!name) return '';
+    const initials = name
       .split(' ')
       .map((n) => n[0])
-      .join('')
-      .toUpperCase();
+      .join('');
+    return initials.toUpperCase();
   }
 }
